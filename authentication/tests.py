f0b539cf8e8deeb1bd8030df9f92c196c817cbac
caleb-xerus <caleb.ho@me.com>
2016-03-14 22:14:15 -0400

    some Account tests

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
-# Create your tests here.
+from authentication.models import Account
+
+class AccountTest(TestCase):
+    def testEmpty(self):
+        with self.assertRaises(TypeError):
+            Account.objects.create_user()
+
+    def testInvalidEmailUser(self):
+        #insufficient arguments
+        with self.assertRaises(ValueError):
+            Account.objects.create_user('test@test.com')
+
+        #not an email address
+        #with self.assertRaises(ValueError):
+        #    Account.objects.create_user('test', username='test')
+
+        #not a valid username
+        with self.assertRaises(ValueError):
+            Account.objects.create_user('test@test.com', username='')
+
+    def testFields(self):
+        a = Account.objects.create_user('test@test.com', username='test')
+
+        self.assertEqual(str(a.email), 'test@test.com', 'Emails do not match')
+        self.assertEqual(str(a.username), 'test', 'Usernames do not match')
+
+    """
+    def testDuplicate(self):
+        #duplicate email and username
+        with self.assertRaises(ValueError):
+            Account.objects.create_user('test@test.com', username='test')
+        #duplicate email
+        with self.assertRaises(ValueError):
+            Account.objects.create_user('test@test.com', username='test1')
+        #duplicate username
+        with self.assertRaises(ValueError):
+            Account.objects.create_user('test1@test.com', username='test')
+    """
+
+    def testLatestCreated(self):
+        b = Account.objects.create_user('test2@test.com', username='test2')
+        self.assertEqual(Account.objects.latest('created_at'),
+                         b,
+                         'Latest not updating correctly'
+        )
